fce9f68c2d52637961ddf7dac9238029433e9c75
thassiov <tvmcarvalho@gmail.com>
2017-03-18 20:09:43 -0300

    Adicionando arquivo

---
+++ launch.js
@@ -0,0 +1,8 @@
+/**
+  * @author Thassio Victor <tvmcarvalho@gmail.com>
+    * @desc Retornar um número de lançamentos futuros de foguetes para o espaço
+    * Esse número vai ser determinado pelo usuário
+  *
+  * @license MIT
+  *
+  */
